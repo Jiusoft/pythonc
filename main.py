5b2e981fb88b5381b6fdeda654370483a0232edc
orlandohuang <orlando.huang8@pm.me>
2022-06-20 10:27:07 +0700

    license

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import os, sys
 args = sys.argv[1:]
 
+def license():
+    print("""MIT License
+
+Copyright (c) 2022 Jiusoft
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.""")
+
 def main():
     if os.path.exists(args[0]):
         code = []
@@ -31,7 +54,9 @@ Usage: pythonc script-to-compile.py
 Arguments:
 -h or --help: Print Help""")
 
-if len(args)==0 or len(args)>1 or args[0]=="-h" or args[0]=="--help":
+if len(args)==0 or len(args)>1 or args[0]=="-h" or args[0]=="-H" or args[0]=="--help":
     help()
+elif args[0]=="-l" or args[0]=="-L" or args[0]=="--license":
+    license()
 else:
     main()
